47d2d66a7f03a129d564e769546dade881c1fd72
VLozova <verkaloz20@gmail.com>
2021-11-22 20:05:58 +0200

    full game

--- WindowsConsoleGameBase/main.cpp
+++ WindowsConsoleGameBase/main.cpp
@@ -1,7 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <algorithm>
 #include "ExampleGame.h"
+
 using namespace std;
 
 int main() {
-	ExampleGame game;
-	return static_cast<int>(game.run());
+	cout << "Welcome to the Minesweeper!" << endl;
+	cout << "Press ARROW KEYS to navigate through the field" << endl;
+	cout << "Press the F key to flag current cell" << endl;
+	cout << "Press the D key to unflag current cell" << endl;
+	cout << "Press the SPACE BAR to open the cell" << endl;
+	cout << "Have a nice game!)" << endl << endl;
+	ExampleGame play;
+	return 0;
 }
